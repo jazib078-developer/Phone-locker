238390ca47e399bfdde807eacef84ae50f886497
jazib078-developer <gamerzitz60@gmail.com>
2026-02-13 16:53:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/phonelocker/device/admin/MyDeviceAdminReceiver.kt
@@ -0,0 +1,9 @@
+package com.example.phonelocker.device.admin
+
+import android.app.admin.DeviceAdminReceiver
+
+class MyDeviceAdminReceiver {
+
+
+    class MyDeviceAdminReceiver : DeviceAdminReceiver()
+}
